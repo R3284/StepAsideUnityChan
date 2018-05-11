5c0dba1421fd22cf4d45b5c53a70e22256a92253
三橋凜太郎 <diary-2013@tees.jp>
2018-05-11 20:56:54 +0900

    課題

---
+++ Assets/kesigonu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class kesigonu : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log("aaaaaaaaaaa");	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter (Collider other)
+	{
+		Debug.Log(other.gameObject.name);
+		Destroy (other.gameObject);
+	}
+}
